4a7c5a13a5bddf97fe7a61087724b0ce69433f1c
Fynn Levine <FKLGTL2010@gmail.com>
2026-05-29 11:14:16 -0700

    feat: Complete Part XVII

--- Tower Defense 2026/Assets/Scripts/TurretSpawner.cs
+++ Tower Defense 2026/Assets/Scripts/TurretSpawner.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TurretSpawner : MonoBehaviour
 {
@@ -6,15 +7,19 @@ public class TurretSpawner : MonoBehaviour
     public GameObject TargetGrid { get; private set; }
     [field: SerializeField]
     public GameObject TurretPrefab { get; private set; }
+    [field: SerializeField]
+    public PlayerController Controller { get; private set; }
 
 
     void OnEnable()
     {
+        Controller.InfoLabel.text = "Select a Tile";
         ListenToTilesIn(TargetGrid);
     }
 
     void OnDisable()
     {
+        Controller.InfoLabel.text = "Click 'Build' To Place A Turret";
         if (TargetGrid == null)
         {
             return;
@@ -26,23 +31,70 @@ public class TurretSpawner : MonoBehaviour
     {
         foreach (TileController tile in grid.GetComponentsInChildren<TileController>())
         {
+            tile.OnCursorEnter.AddListener(ShowInfo);
+            tile.OnCursorExit.AddListener(HandleTileExited);
             tile.OnCursorClicked.AddListener(SpawnTurret);
         }
     }
     public void SpawnTurret(TileController tileController)
     {
         if (tileController.IsOccupied) { return ;}
-        GameObject newTurret = Instantiate(TurretPrefab);
-        newTurret.transform.position = tileController.transform.position;
-        tileController.IsOccupied = true;
+        if (CanSpawn(tileController))
+        {
+            GameObject newTurret = Instantiate(TurretPrefab);
+            newTurret.transform.position = tileController.transform.position;
+            tileController.IsOccupied = true;
+            Controller.Gold -= 50;
+            gameObject.SetActive(false);
+        }
     }
 
     public void StopListeningToTilesIn(GameObject grid)
     {
         foreach (TileController tile in grid.GetComponentsInChildren<TileController>())
         {
+            tile.OnCursorEnter.RemoveListener(ShowInfo);
+            tile.OnCursorExit.RemoveListener(HandleTileExited);
             tile.OnCursorClicked.RemoveListener(SpawnTurret);
         }
+    }
+
+    public bool CanSpawn(TileController tileController)
+    {
+        if (tileController.IsOccupied)
+        {
+            return false;
+        }
+
+        if (Controller.Gold < 50)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void ShowInfo(TileController tileController)
+    {
+        if (tileController.IsOccupied)
+        {
+            Controller.InfoLabel.text = "Unable To Build Here";
+        }
+        else if (Controller.Gold < 50)
+        {
+            Controller.InfoLabel.text = "<color=red>Insufficient Gold</color>";
+        }
+        else
+        {
+            Controller.InfoLabel.text = "50 Gold - Place Turret";
+        }
+    }
+
+    public void HandleTileExited(TileController tileController)
+    {
+        Controller.InfoLabel.text = "Select a Tile";
     }
 
 }
